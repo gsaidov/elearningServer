f4905a67df8133610bf28a8e3723bae3542258c0
gsaidov <gulomjonsaidov@gmail.com>
2021-03-31 22:28:45 -0700

    /course/courseId/reviews and /course/courseId/reviews/reviewId routes

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -77,4 +77,156 @@ courseRouter
       .catch((err) => next(err));
   });
 
+// /course/courseId/reviews route
+courseRouter
+  .route("/:courseId/reviews")
+  .get((req, res, next) => {
+    Course.findById(req.params.courseId)
+      .then((course) => {
+        if (course) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(course.reviews);
+        } else {
+          err = new Error(`Course ${req.params.courseId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    Course.findById(req.params.courseId)
+      .then((course) => {
+        if (course) {
+          course.reviews.push(req.body);
+          course
+            .save()
+            .then((course) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(course.reviews);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`Course ${req.params.courseId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `PUT operation not supported on /courses${req.params.courseId}/reviews`
+    );
+  })
+  .delete((req, res, next) => {
+    Course.findById(req.params.courseId)
+      .then((course) => {
+        if (course) {
+          for (let i = course.reviews.length - 1; i >= 0; i--) {
+            course.reviews.id(course.reviews[i]._id).remove();
+          }
+          course
+            .save()
+            .then((course) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(course.reviews);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`Course ${req.params.courseId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
+// /course/courseId/reviews/:reviewId route
+courseRouter
+  .route("/:courseId/reviews/:reviewId")
+  .get((req, res, next) => {
+    Course.findById(req.params.courseId)
+      .then((course) => {
+        if (course && course.reviews.id(req.params.reviewId)) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(course.reviews.id(req.params.reviewId));
+        } else if (!course) {
+          err = new Error(`Course ${req.params.courseId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Course ${req.params.reviewId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /courses/${req.params.courseId}/reviews/${req.params.reviewId}`
+    );
+  })
+  .put((req, res, next) => {
+    Course.findById(req.params.courseId)
+      .then((course) => {
+        if (course && course.reviews.id(req.params.reviewId)) {
+          if (req.body.rating) {
+            course.reviews.id(req.params.reviewId).rating = req.body.rating;
+          }
+          if (req.body.text) {
+            course.reviews.id(req.params.reviewId).text = req.body.text;
+          }
+          course
+            .save()
+            .then((course) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(course);
+            })
+            .catch((err) => next(err));
+        } else if (!course) {
+          err = new Error(`Course ${req.params.courseId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Course ${req.params.reviewId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Course.findById(req.params.courseId)
+      .then((course) => {
+        if (course && course.reviews.id(req.params.reviewId)) {
+          course.reviews.id(req.params.reviewId).remove();
+          course
+            .save()
+            .then((course) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(course);
+            })
+            .catch((err) => next(err));
+        } else if (!course) {
+          err = new Error(`Course ${req.params.courseId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Course ${req.params.reviewId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
+
 module.exports = courseRouter;
